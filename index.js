02794a1842d70d5f8b83b9cbdb72f134ac0ccc7c
LAB_A_REAL <laba@continental.edu.pe>
2024-06-08 14:46:18 -0500

    s2

--- index.js
+++
@@ -1,6 +0,0 @@
-function mensajeAleatorio() {
-    var mensajes = ["¡Te atrapé!", "¡Se escapó!"];
-    var indice = Math.floor(Math.random() * mensajes.length);
-    var mensaje = mensajes[indice];
-    document.getElementById("mensaje").textContent = mensaje;
-}
